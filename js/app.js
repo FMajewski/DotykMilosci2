1f23c2ddd26bdca0845d46be1743ec03b259183a
FilipM <majewski2405@gmail.com>
2019-09-01 23:18:06 +0200

    modal look changed, mediaqueries added, small html changes

--- js/app.js
+++ js/app.js
@@ -43,7 +43,6 @@ document.addEventListener("DOMContentLoaded", function () {
         modal.style.display = "block";
     }
 
-
 // When the user clicks on <span> (x), close the modal
     span.onclick = function() {
         modal.style.display = "none";
